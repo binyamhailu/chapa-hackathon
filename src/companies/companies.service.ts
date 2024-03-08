566be3d9a13dadfdd0da319487865dc4a8bc8d8c
Binyam.Gebrekidan <binyam.gebrekidan@partner.safaricom.et>
2024-03-08 07:15:20 +0300

    added get companies

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Company } from './entities/company.entity';
 
 @Injectable()
 export class CompaniesService {
+  constructor(
+    
+    @InjectRepository(Company)
+    private companyRepository: Repository<Company>,
+
+
+  ) { }
   create(createCompanyDto: CreateCompanyDto) {
     return 'This action adds a new company';
   }
 
   findAll() {
-    return `This action returns all companies`;
+    return this.companyRepository.find();
   }
 
   findOne(id: number) {
