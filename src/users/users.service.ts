6066b19aa1ec18c721fd0c93a73ab35b41c85c66
Binyam.Gebrekidan <binyam.gebrekidan@partner.safaricom.et>
2024-03-08 07:22:24 +0300

    added mockup data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -20,38 +20,38 @@ export class UsersService {
 
   ) { }
   async create(createUserDto: CreateUserDto) {
-    // Assuming you're using TypeORM
     const user1 = await this.userRepository.save({ name: 'John Doe', phone: '123456789', pin: "1234" });
     const user2 = await this.userRepository.save({ name: 'Alice Smith', phone: '987654321', pin: "5678" });
 
 
-    const company1 = await this.companyRepository.save({ name: 'CHAPA Inc.',  avlShares:500, pricePerShare: 1000, totalShares: 500  });
+    const company0 = await this.companyRepository.save({ name: 'Chapa SC.', avlShares: 500, pricePerShare: 1500, totalShares: 500 });
+    const company1 = await this.companyRepository.save({ name: 'COOP Inc.', avlShares: 500, pricePerShare: 1500, totalShares: 500 });
+    const company2 = await this.companyRepository.save({ name: 'Ethiopia AirLines', avlShares: 500, pricePerShare: 1800, totalShares: 500 });
+    const company3 = await this.companyRepository.save({ name: 'Ethio Telecom', avlShares: 500, pricePerShare: 1900, totalShares: 500 });
 
-    // Save the share and associate it with the company
 
-    // Now create ownerships for the shares
     await this.ownershipRepository.save({ sharesOwned: 50, user: user1, company: company1 });
-    
+
     company1.avlShares=company1.totalShares - 50
     const company12 = await this.companyRepository.save(company1);
-    
+
     await this.ownershipRepository.save({ sharesOwned: 20, user: user2, company: company1 });
-    
+
     company12.avlShares = company12.avlShares - 20
-    
+
     await this.companyRepository.save(company12);
 
     return this.userRepository.find({
-      
-        relations: ['ownedShares', 'ownedShares.company']
-      
+
+      relations: ['ownedShares', 'ownedShares.company']
+
     });
   }
   async login(loginDto: LoginDto) {
     const user = await this.userRepository.findOne({
       where: { phone: loginDto.phone, pin: loginDto.pin }
     });
-  
+
     if (!user) {
       return {
         status: false,
@@ -59,20 +59,20 @@ export class UsersService {
         data: null,
       };
     }
-  
+
     return {
       status: true,
       message: `User Login Successfully`,
       data: user,
     };
   }
-  
-  async signup(signUpDto:SignUpDto){
-     const user  = await this.userRepository.findOne({
-      where: {phone: signUpDto.phone}
-     })
-     if (!user) {
-      const user= await this.userRepository.save(signUpDto)
+
+  async signup(signUpDto: SignUpDto) {
+    const user = await this.userRepository.findOne({
+      where: { phone: signUpDto.phone }
+    })
+    if (!user) {
+      const user = await this.userRepository.save(signUpDto)
       return {
         "status": true,
         "message": `user Created Successfully!`,
@@ -110,7 +110,8 @@ export class UsersService {
   async findOne(id: number) {
     const user = await this.userRepository.findOne({
       where: { id: id },
-      relations: ['ownedShares', 'ownedShares.company']    });
+      relations: ['ownedShares', 'ownedShares.company']
+    });
 
     if (!user) {
       return {
